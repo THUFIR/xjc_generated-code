f6c1a17bc7eafbc5dbcbec24d99801bf85bce032
thufir <hawat.thufir@gmail.com>
2019-01-17 14:20:21 -0800

    incremental

--- src/main/java/xjc_generated/code/driver/App.java
+++ src/main/java/xjc_generated/code/driver/App.java
@@ -2,27 +2,47 @@ package xjc_generated.code.driver;
 
 import generated.MLIspec;
 import java.io.File;
+import java.io.FileInputStream;
 import java.net.URI;
 import java.util.Properties;
 import java.util.logging.Logger;
+import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
+import javax.xml.transform.stream.StreamSource;
 
 public class App {
 
     private static final Logger LOG = Logger.getLogger(App.class.getName());
     private Properties properties = new Properties();
+    private MLIspec mliSpec = null;
 
     public static void main(String[] args) throws Exception {
-        new App().readAndPrint();
+        new App().unmarshall();
     }
 
-    public void readAndPrint() throws Exception {
+    private void unmarsklhall() throws Exception {
+    }
+
+    private MLIspec unmarshall() throws Exception {
+        properties.loadFromXML(App.class.getResourceAsStream("/properties.xml"));
+        URI uri = new URI(properties.getProperty("mli_input"));
+        File file = new File(uri);
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+        StreamSource streamSource = new StreamSource();
+        streamSource.setInputStream(fileInputStream);
+
+        mliSpec = JAXB.unmarshal(streamSource, MLIspec.class);
+        LOG.info(mliSpec.toString());
+        return mliSpec;
+    }
+
+    private void marshall() throws Exception {
         properties.loadFromXML(App.class.getResourceAsStream("/properties.xml"));
         URI uri = new URI(properties.getProperty("output"));
         File file = new File(uri);
 
-        MLIspec mliSpec = new MLIspec();
         JAXBContext jaxbContext = JAXBContext.newInstance(MLIspec.class);
         Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
         jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
