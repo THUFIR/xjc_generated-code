5cb9b78babc761e55a36aa60a7c374cca2a2cb20
thufir <hawat.thufir@gmail.com>
2019-01-17 13:58:12 -0800

    incremental

--- src/main/java/xjc_generated/code/driver/App.java
+++ src/main/java/xjc_generated/code/driver/App.java
@@ -1,7 +1,14 @@
 package xjc_generated.code.driver;
 
+import generated.Element;
+import generated.MLIspec;
+import generated.ObjectFactory;
+import java.io.File;
+import java.net.URI;
 import java.util.Properties;
 import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
 
 public class App {
 
@@ -14,7 +21,26 @@ public class App {
 
     public void readAndPrint() throws Exception {
         properties.loadFromXML(App.class.getResourceAsStream("/properties.xml"));
-        LOG.info("hi");
+        URI uri = new URI(properties.getProperty("output"));
+        File file = new File(uri);
+
+        ObjectFactory objectFactory = new ObjectFactory();
+        MLIspec mliSpec = objectFactory.createMLIspec();
+        mliSpec.setDescription("new mli");
+        Element value = new Element();
+        value.setId("some other thing");
+        mliSpec.setElement(value);
+        mliSpec.setId("identification string");
+        mliSpec.setTypename("newType");
+
+        LOG.info(mliSpec.toString());
+
+        JAXBContext jaxbContext = JAXBContext.newInstance();
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        marshaller.marshal(mliSpec, file);
+
     }
 
 }
