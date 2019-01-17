60d0880f37da09e5ba89d91d58f1d96440af7ff6
thufir <hawat.thufir@gmail.com>
2019-01-17 13:41:32 -0800

    incremental

--- src/main/java/xjc_generated/code/driver/App.java
+++ src/main/java/xjc_generated/code/driver/App.java
@@ -1,20 +1,20 @@
 package xjc_generated.code.driver;
 
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class App {
-    
-        private static final Logger LOG = Logger.getLogger(App.class.getName());
 
-    
-    public static void main(String[] args) {
-       new App().readAndPrint();
+    private static final Logger LOG = Logger.getLogger(App.class.getName());
+    private Properties properties = new Properties();
+
+    public static void main(String[] args) throws Exception {
+        new App().readAndPrint();
     }
-    
-    
-    public void readAndPrint() {
+
+    public void readAndPrint() throws Exception {
+        properties.loadFromXML(App.class.getResourceAsStream("/properties.xml"));
         LOG.info("hi");
     }
 
-    
 }
